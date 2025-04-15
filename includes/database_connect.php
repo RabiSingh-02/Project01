3630e51930ccce3c4a8c2e2342e72f253d2ab3de
Rabi Singh <rabisingh6770@gmail.com>
2025-04-15 16:12:33 +0530

    Delete includes directory

--- includes/database_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect("127.0.0.1", "root", "SQLr@b!002", "dbms");
-
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL! Please contact the admin.";
-    return;
-}
-?>
