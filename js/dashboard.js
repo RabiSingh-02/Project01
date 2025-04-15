18abbb17a1b837b4d12a5e4a8d75176e9be71edf
Rabi Singh <rabisingh6770@gmail.com>
2025-04-15 16:26:32 +0530

    Create dashboard.js

---
+++ js/dashboard.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", function () {
+    var is_interested_images = document.getElementsByClassName("is-interested-image");
+    Array.from(is_interested_images).forEach(element => {
+        element.addEventListener("click", function (event) {
+            var XHR = new XMLHttpRequest();
+            var property_id = event.target.getAttribute("property_id");
+
+            XHR.addEventListener("load", remove_interested_success);
+
+            XHR.addEventListener("error", on_error);
+
+            XHR.open("GET", "api/toggle_interested.php?property_id=" + property_id);
+
+            XHR.send();
+
+            document.getElementById("loading").style.display = 'block';
+            event.preventDefault();
+        });
+    });
+});
+
+var remove_interested_success = function (event) {
+    document.getElementById("loading").style.display = 'none';
+
+    var response = JSON.parse(event.target.responseText);
+    if (response.success) {
+        var property_id = response.property_id;
+
+        document.getElementsByClassName("property-id-" + property_id)[0].style.display = 'none';
+    }
+};
